84717478e6765c8ed0c74f227b8ad7b87748884d
Melisa Paniagua <melisa.paniagua@gmail.com>
2022-08-19 00:20:28 -0300

    primera entrega clase de React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './estilos.css';
 
 function App() {
+let imagen = "t5.png";
+let titulo = "Impresiones 3d";
+let descripcion = " Are You A Fan Of Astronomy? An Astronaut Dreamer? This is you place"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "container">
+      <div className = "row">
+        <div className= "col-md-4 offset-md-2">
+          <img src={imagen} alt = {titulo} className= "img-fluid"/>
+        </div>
+        <div className= "col-md-4">
+          <h1>{titulo}</h1>
+          <p>{descripcion}</p>
+        </div>
+      </div>
     </div>
+
   );
 }
 
